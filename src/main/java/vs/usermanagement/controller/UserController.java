7d53e24fa5421b6d8784ad1af68d5b1192e16951
Vasilis Souvatzis <vasouv@gmail.com>
2019-04-08 23:38:18 +0300

    Update UserController.java

--- src/main/java/vs/usermanagement/controller/UserController.java
+++ src/main/java/vs/usermanagement/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vs.usermanagement.entities.User;
 import vs.usermanagement.exception.UserNotFoundException;
@@ -13,6 +14,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
+@RequestMapping("user")
 public class UserController {
 
     private UserService userService;
@@ -21,12 +23,12 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("user")
+    @GetMapping
     public ResponseEntity getAll() {
         return ResponseEntity.ok(userService.findAll());
     }
 
-    @GetMapping("user/{id}")
+    @GetMapping("{id}")
     public ResponseEntity findUser(@PathVariable int id) {
         var found = userService.findById(id);
         if (found.isPresent()) {
